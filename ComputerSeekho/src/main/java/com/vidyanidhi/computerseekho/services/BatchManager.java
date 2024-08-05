5e7fbf4fd700f82f2c17dfe92731356326a055bc
swapnilD31 <swapnildeore004@gmail.com>
2024-08-05 12:57:57 +0530

    Batch and course Controller added

---
+++ ComputerSeekho/src/main/java/com/vidyanidhi/computerseekho/services/BatchManager.java
@@ -0,0 +1,16 @@
+package com.vidyanidhi.computerseekho.services;
+
+import java.util.List;
+
+import com.vidyanidhi.computerseekho.entities.Batch;
+
+public interface BatchManager {
+	void save(Batch ref);
+	List<Batch> getAll();
+	Batch getBatch(int batchno);
+	List<Batch> getUpcomingBatch();
+	List<Batch> getCurrentBatch();
+	List<Batch> getPastBatch();
+	List<Batch> getBatchByName(String batchName);
+	List<Batch> getBatchByCourseId(int cid);
+}
